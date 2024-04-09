b70a593c4a2fdacaecc10ddabcaaf18e45922790
EmreShakir1 <124499937+EmreShakir1@users.noreply.github.com>
2024-04-10 01:20:53 +0300

    Created repository interface and class

---
+++ MiniFootballApp.Infrastucture/Data/Common/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniFootballApp.Infrastucture.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext dbContext;
+
+        public Repository(MiniFootballDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return dbContext.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>().AsQueryable();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking().AsQueryable();
+        }
+    }
+}
